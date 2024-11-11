a28d5b9c459252fe36ebda0fae88965c2a84bc61
the-nmercer <thenicholasmercer@gmail.com>
2024-11-11 17:05:59 -0600

    Associated Product and Category models.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+belongs_to :category
+
   def self.ransackable_attributes(auth_object = nil)
     ["name", "description", "category", "price", "stock_quantity", "created_at", "updated_at"]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_11_225242) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_11_230439) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_11_225242) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "decription"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
