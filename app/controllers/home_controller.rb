1be2c17a2b25b8b9527bc879f7086433eb4e6091
the-nmercer <thenicholasmercer@gmail.com>
2024-11-24 20:57:32 -0600

    Added recent products and recently updated product views to home index and products index.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
     @products = Product.all
+
+    @new_products = Product.where('created_at >= ?', 3.days.ago).presence || []
+    @recently_updated_products = Product.where('updated_at >= ?', 3.days.ago)
+                                         .where('created_at < ?', 3.days.ago).presence || []
+
+    @random_products = Product.all.sample(10)
+
+    if params[:filter] == 'new'
+      @products = @new_products
+    elsif params[:filter] == 'recently_updated'
+      @products = @recently_updated_products
+    end
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    if params[:filter] == 'new'
+      @products = @products.where('created_at >= ?', 3.days.ago)
+    elsif params[:filter] == 'recently_updated'
+      @products = @products.where('updated_at >= ?', 3.days.ago)
+      @products = @products.where('created_at < ?', 3.days.ago)
+    end
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.find_by(id: params[:id])
+
+    unless @product
+      redirect_to products_path, alert: 'Product not found'
+    end
   end
 end
