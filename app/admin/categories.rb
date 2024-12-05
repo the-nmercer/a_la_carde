6df4107b7002a00207a6ce88f4c3b75161f9e7d0
the-nmercer <thenicholasmercer@gmail.com>
2024-12-05 05:24:09 -0600

    Added ability to create or edit categories from the admin page.

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,18 +1,24 @@
 ActiveAdmin.register Category do
+  permit_params :name, :description, :text
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :text
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :text]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs "Category Details" do
+      f.input :name
+      f.input :description
+    end
+    f.actions
+  end
+
+  filter :name
+  filter :description
+  filter :created_at
 end
